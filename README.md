040732712ebed76a81e8a996538d6c101ec84e1c
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-05-04 12:07:24 -0400

    Adding telemetry

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dashboard Data Creator
+This tool creates fake data to aid the development of the endpoint dashboard
+
+## Usage
+```bash
+python create.py <path to configuration file>
+```
--- create.py
+++ create.py
@@ -10,7 +10,10 @@ import uuid
 import random
 import faker
 import pytz
-import geopy
+import collections
+
+events_index = 'events_index'
+metrics_index = 'metrics_index'
 
 
 def parse_args():
@@ -19,28 +22,38 @@ def parse_args():
     return parser.parse_args()
 
 
-def create_template(config, es):
-    if 'template' not in config:
-        return
-
-    print('Creating template')
+def put_template(es, template_name, index_patterns):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
-    template_path = os.path.join(cur_dir, config['template'])
+    template_path = os.path.join(cur_dir, template_name)
     with open(template_path, 'r') as t:
         template = json.load(t)
 
-    template['index_patterns'] = config['index_patterns']
-    es.indices.put_template(name='dashboard_template', body=template, create=False)
+    template['index_patterns'] = index_patterns
+    filename = os.path.splitext(template_name)[0]
+    es.indices.put_template(name='dashboard_template_{}'.format(filename), body=template, create=False)
+
+
+def create_template(config, es):
+    print('Creating templates')
+    if 'events_template' in config:
+        put_template(es, config['events_template'], config['events_index_patterns'])
+    if 'metrics_template' in config:
+        put_template(es, config['metrics_template'], config['metrics_index_patterns'])
 
 
 def gen_semver():
     return '{}.{}.{}'.format(random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
 
 
+windows = 'Windows'
+mac = 'macOS'
+linux = 'Linux'
+oses = [windows, mac, linux]
+
+
 def gen_host_info():
 
     fake = faker.Faker()
-    oses = ['Windows', 'macOS', 'Linux']
     host = {
         'agent': {
             'id': str(uuid.uuid4()),
@@ -58,7 +71,64 @@ def gen_host_info():
     return host
 
 
-def random_date(days_prior=3):
+gb = 1073741824
+
+
+def gen_metrics(os_info, event_info):
+    if os_info == windows:
+        drive = 'c:\\'
+    else:
+        drive = '/var/'
+
+    system_uptime = random.randint(1, 365 * 24 * 60 * 60)
+    timestamp = random_date()
+    data = {
+        '@timestamp': timestamp.isoformat() + 'Z',
+        'event': {
+            'category': ['host'],
+            'id': uuid.uuid4(),
+            'kind': 'metrics',
+        },
+        'endpoint': {
+            'system': {
+                'uptime': {
+                    # seconds in a year
+                    'endpoint': random.randint(1, system_uptime),
+                    'system': system_uptime
+                },
+                'cpu': {
+                    'endpoint': {
+                        'mean': random.uniform(1.0, 99.0),
+                        'latest': random.uniform(1.0, 99.0),
+                        'histogram': [random.randint(0, 60 * 5) for i in range(20)]
+                    }
+                },
+                'memory': {
+                    'endpoint': {
+                        'private': {
+                            'mean': random.randint(1, gb),
+                            'latest': random.randint(1, gb)
+                        }
+                    }
+                },
+                'disks': [
+                    {
+                        'mount': drive,
+                        'free': random.randint(gb*50, gb*300),
+                        'total': gb*500
+                    }
+                ],
+                'events': {
+                    'since_start': event_info,
+                    'backlog_size': random.randint(0, 200000)
+                }
+            }
+        }
+    }
+    return data
+
+
+def random_date(days_prior=9):
     delta = datetime.timedelta(days=days_prior)
     start = datetime.datetime.now(pytz.timezone('EST')) - delta
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
@@ -72,7 +142,7 @@ def gen_event():
         'event': ['network', 'file', 'driver', 'process', 'registry'],
         'alert': ['malware']
     }
-    cat_key = random.choices(list(categories.keys()), [.99, .01])[0]
+    cat_key = random.choices(list(categories.keys()), [.60, .40])[0]
     #datetime.datetime.now(pytz.timezone('EST')).isoformat() + 'Z',
     timestamp = random_date()
     return {
@@ -103,24 +173,36 @@ def gen_locations(num_locations):
 
 def generate_events(config):
     num_hosts = random.randint(35, 60)
-    num_locations = random.randint(3, 10)
+    event_info = collections.defaultdict(int)
+    #num_locations = random.randint(3, 10)
     #num_locations = 3
-    locations = gen_locations(num_locations)
+    #locations = gen_locations(num_locations)
     for i in range(num_hosts):
-        num_events = random.randint(1, 500)
+        num_events = random.randint(1, 100)
         host = gen_host_info()
-        loc = random.choice(locations)
-        host['host'].update(loc)
+        #loc = random.choice(locations)
+        #host['host'].update(loc)
         for j in range(num_events):
             event = gen_event()
+            event_kind = event['event']['kind']
+            event_info[event_kind] += 1
             event.update(host)
 
             bulk_request = {
-                '_index': config['index'],
+                '_index': config[events_index],
             }
             bulk_request.update(event)
             yield bulk_request
 
+        num_metrics = random.randint(1, 500)
+        for j in range(num_metrics):
+            metrics = gen_metrics(host['host']['os']['name'], event_info)
+            metrics.update(host)
+            bulk_request = {
+                '_index': config[metrics_index],
+            }
+            bulk_request.update(metrics)
+            yield bulk_request
 
 def insert_events(config, es):
     helpers.bulk(es, generate_events(config))
@@ -130,8 +212,10 @@ def del_previous_index(config, es):
     if not config['del_previous']:
         return
 
-    print('Deleting previous index {}'.format(config['index']))
-    es.indices.delete(index=config['index'], ignore=[404])
+    print('Deleting previous index {}'.format(config[events_index]))
+    es.indices.delete(index=config[events_index], ignore=[404])
+    print('Deleting previous index {}'.format(config[metrics_index]))
+    es.indices.delete(index=config[metrics_index], ignore=[404])
 
 
 def main():
